b92ea7c33a91e54587886e0bbb620bf3e9dd88f5
suresh01432 <mudavathsuresh0143@gmail.com>
2024-06-29 15:17:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my web app!');
+    res.send('Welcome to suresh web app!');
 });
 
 app.listen(port, () => {
